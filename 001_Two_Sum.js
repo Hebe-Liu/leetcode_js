f535686154847fabfda3bcaa2542caa1f7d6c50d
Hebe-Liu <41422256+Hebe-Liu@users.noreply.github.com>
2019-07-12 13:26:29 +0800

    Update 001_Two_Sum.js

--- 001_Two_Sum.js
+++ 001_Two_Sum.js
@@ -1,8 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+
 var twoSum = function(nums, target) {
-   var map = {};
-   for(var i=0; i<nums.length; i++) {       
-	   for(var j =i+1; j<nums.length; j++) {
-            if (nums[i] + nums[j]==target) return[i,j];
+   for(var i=0; i<nums.length; i++) {
+	   for(var j=i+1; j<nums.length; j++) {
+		   if (nums[i] + nums[j] == target)	return[i,j];
         }
 	}
 };
